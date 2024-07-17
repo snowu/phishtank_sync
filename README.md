255529198d11a6448efa10378f42bc9e2cb7539d
snowu <zancato.t@gmail.com>
2024-07-18 01:01:54 +0200

    cleanup and final touches, more info in readme, requirements.txt

--- README.md
+++ README.md
@@ -1,16 +1,46 @@
+Build docker image from repo:
+
+    From inside the repo folder:
+
+    sudo docker build -t phishtank-service .
+    sudo docker run -p 8000:8000 phishtank-service
+
+    Will instantiate a docker image on port 8000.
+
+Download docker image:
+    
+    If you want to just download and run the image, you can find it here
+    https://hub.docker.com/repository/docker/snowu/rapid7/general
+
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+*API Usage*
+
 Download report:
 
-    Report takes in JSON since to_time can be Optional and having it in the url means using an additional url parameter like 
-    url_to_api/20240701?to_time=20240717 and it feels a bit wrong to do so since it's unintuitive so i chose JSON for this
+    Report takes in JSON as parameters since to_time can be optional and having it in the url means using an additional url parameter like url_to_api/20240701?to_time=20240717
+    It feels a bit wrong to do so since it's unintuitive that two values with the same logic and formatting use different way to integrate into API so i chose JSON for this
 
-    Example curl will download report to report.json, from the first of July to the 17th. Date are accepted in iso8601 format, both dates in the example are fine
-    If timezone is not included, we'll adapt since phishtank uses UTC by default
+    Example curl will download report to report.json, from the first of July to the 17th. Date are accepted in iso8601 format, both dates format in the example are fine
+    If timezone is not included, API will set timezone to UTC, since phishtank used UTC by default
+    In addition, if a specific time is not passed, API will count the whole day of to_time, which is more intuitive. If i put 20240717, i expect results from the whole day as well.
+    So, unless a time is specified (with T%H:%M:%S), the results will be from 00:00 of from_time to 23:59:59 of to_time
 
+    It's also possible to pass timezone offsets, they will be converted to UTC, 
+
+        so  "20240717T00:00:00-02:00" will be 2024-07-17 02:00:00
+
+        and "20240717T00:00:00+02:00" will be 2024-07-16 22:00:00
+
+    This is a tad counterintuitive until you realize that it counts as an offset from UTC,so +02 means a zone where it's 2 hours ahead of UTC which means to convert to UTC we move the clock back 2 hours.
+    I suggest just passing dates and times without any timezone, as the API will convert and use UTC as basis
+                                                                                    
     curl -o "report.json" -X "GET" --json '{"from_time": "20240701", "to_time": "2024-07-17"}' "http://localhost:8000/download_report"
 
 Search domain:
 
-    Pretty straightforward, API checks if {domain_to_search} is present in the url and returns a list of URLs that matched it. 
-    In this case the argument is inline since it's just the one and it has to be present and FastAPI throws a 404 if the url is without {domain_to_search}
+    Pretty straightforward, API checks (case unsensitive) if {domain_to_search} is present in the url and returns a list of URLs that matched it. 
+    In this case the argument is inline since it's just the one and it has to be present since FastAPI throws a 404 if the url is without {domain_to_search}
 
     curl -o "domain_search.json" -X "GET" "http://localhost:8000/search_domain/{domain_to_search}"
--- phishtank.py
+++ phishtank.py
@@ -1,9 +1,8 @@
-# main.py
 import asyncio
 import aiohttp
 import csv
-import tldextract
 import io
+import tld
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from datetime import datetime, timezone, timedelta
@@ -17,19 +16,29 @@ PHISHTANK_URL = "http://data.phishtank.com/data/online-valid.csv"
 CSV_FILE = "verified_online.csv"
 phish_data = []
 
+
 class DownloadReportRequest(BaseModel):
     from_time: str
     to_time: Optional[str] = None
 
-def iso_to_utc(iso_date):
+
+def iso_to_utc(iso_date: str, include_day: bool = False) -> datetime:
     dt = datetime.fromisoformat(iso_date)
+
     if dt.tzinfo is None:
         dt = dt.replace(tzinfo=timezone.utc)
     else:
         dt = dt.astimezone(timezone.utc)
+
+    time_not_specified = ':' not in iso_date
+
+    if include_day and time_not_specified:
+        dt += timedelta(hours=23, minutes=59, seconds=59)
+
     return dt
 
-def load_local_csv():
+
+def load_local_csv() -> list:
     try:
         with open(CSV_FILE, "r") as csvfile:
             reader = csv.DictReader(csvfile)
@@ -37,6 +46,7 @@ def load_local_csv():
     except IOError as e:
         print(f"Error reading local CSV file: {e}")
         return []
+
 
 async def fetch_phishtank_data():
     global phish_data
@@ -51,68 +61,99 @@ async def fetch_phishtank_data():
                 reader = csv.DictReader(io.StringIO(content))
                 phish_data = list(reader)
             else:
-                raise aiohttp.ClientError(f"Failed to fetch data: HTTP {response.status}")
+                raise aiohttp.ClientError(
+                    f"Failed to fetch data: HTTP {response.status}"
+                )
+
 
 async def update_phish_data():
     global phish_data
+
     try:
-        phish_data = await fetch_phishtank_data()
+        await fetch_phishtank_data()
     except aiohttp.ClientError as e:
         print(e)
         print(f"Using {CSV_FILE} file instead")
         phish_data = load_local_csv()
+    finally:
+        print(f"Fetched {len(phish_data)} records from Phishtank")
 
-    print(f"Fetched {len(phish_data)} records from Phishtank")
 
 async def scheduled_fetch():
     while True:
         await update_phish_data()
         await asyncio.sleep(3600)
 
+
 @app.on_event("startup")
 async def startup_event():
     asyncio.create_task(scheduled_fetch())
 
-@app.get("/download_report")
+
+@app.get("/download_report", response_class=JSONResponse)
 async def download_report(request: DownloadReportRequest):
     try:
-        from_time, to_time = request.from_time, request.to_time
-        from_datetime = iso_to_utc(from_time)
-        to_datetime = iso_to_utc(to_time) if to_time else datetime.now(timezone.utc)
+        from_datetime = iso_to_utc(request.from_time)
+
+        if from_datetime > datetime.now(timezone.utc):
+            raise ValueError("from_time cannot be in the future.")
+
+        if request.to_time:
+            to_datetime = iso_to_utc(request.to_time, include_day=True)
+        else:
+            datetime.now(timezone.utc)
+
         if from_datetime > to_datetime:
-            raise ValueError(f"from_time cannot be bigger than to_time.")
+            raise ValueError("from_time cannot be bigger than to_time.")
 
-        time_range = to_datetime - from_datetime
-        urls = [
-            entry["url"] for entry in phish_data
-            if timedelta(0) <= (datetime.fromisoformat(entry['submission_time']) - from_datetime) <= time_range
-        ]
+        tld_counter = Counter()
+        urls = []
 
-        tld_counter = Counter(tldextract.extract(url).suffix for url in urls)
+        for entry in phish_data:
+
+            url = entry["url"]
+
+            submission_time = datetime.fromisoformat(entry["submission_time"])
+            if not from_datetime <= submission_time <= to_datetime:
+                continue
+
+            urls.append(url)
+
+            try:
+                parsed_tld = tld.get_tld(url)
+            except tld.exceptions.TldDomainNotFound:
+                continue
+
+            tld_counter[parsed_tld] += 1
 
         return JSONResponse({
-            # "urls": urls,
             "total_results": len(urls),
-            "tlds": tlds,
+            "urls": urls,
             "tlds": tld_counter.most_common(),
         })
 
     except ValueError as e:
-        raise HTTPException(status_code=400, detail=f"Invalid date format: {str(e)}")
+        raise HTTPException(
+            status_code=400, detail=f"Invalid date format: {str(e)}"
+        )
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"An unexpected error occurred: {str(e)}")
+        raise HTTPException(
+            status_code=500, detail=f"An unexpected error occurred: {str(e)}"
+        )
 
-@app.get("/search_domain/{domain}")
+
+@app.get("/search_domain/{domain}", response_class=JSONResponse)
 async def search_domain(domain: str):
     matching_entries = [
         entry['phish_detail_url'] for entry in phish_data
-        if domain in entry['url']
+        if domain.lower() in entry['url'].lower()
     ]
 
     if not matching_entries:
         raise HTTPException(status_code=404, detail="Domain not found")
 
     return JSONResponse({"matching_urls": matching_entries})
+
 
 if __name__ == "__main__":
     import uvicorn
